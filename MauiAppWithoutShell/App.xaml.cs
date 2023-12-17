141b34a8d8f2374cdba02e5279a1b139417ff9d6
Atex Inspect <atexinspect@hotmail.com>
2023-12-17 09:17:19 +0000

    Hamburger menu now displayed.

--- MauiAppWithoutShell/App.xaml.cs
+++ MauiAppWithoutShell/App.xaml.cs
@@ -6,8 +6,7 @@
         {
             InitializeComponent();
 
-            //MainPage = new MainPage();
-            MainPage = new NavigationPage(new Page1());
+            MainPage = new MainPage();
         }
     }
 }
